1c45bc158418aecc22470778de15dfd10ee8260c
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2026-09-03 21:05:35 +0000

    Update mp3compress128.sh

--- mp3compress128.sh
+++ mp3compress128.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# compress mp3 to 128kbps
+
+# Requires
+# ffmpeg installed
+# lame installed
+
 for file in *.mp3; do lame --preset 128 -ms -h "$file"; done
 
 for i in *.mp3.mp3 ; do
