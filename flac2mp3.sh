9b869da1970478c348d9b9091062f09f43745e24
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2026-09-03 21:04:42 +0000

    Update flac2mp3.sh

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# flac2mp3 file converter
+
+# Requires
+# ffmpeg installed
+# lame installed
+
 for a in ./*.flac; do
   < /dev/null ffmpeg -i "$a" -qscale:a 0 "${a[@]/%flac/mp3}"
 done
